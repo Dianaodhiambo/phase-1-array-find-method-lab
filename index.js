3d78d35a986af5182f09f646af964ad47ab4adb4
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-08-20 22:03:28 +0300

    Completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const superbowlWin = (arr) => {
+    let year;
+    arr.find((element) => {
+        if (element.result === "W") {
+            year = element.year;
+            return true;
+        }
+        return false;
+    });
+    return year;
+}; 
